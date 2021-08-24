dcd469c4308a0ef4c093e7b81ea8d24f80df2112
allinh1 <dl.nguyen1322@gmail.com>
2021-08-24 14:15:27 +1000

    first commit from local

---
+++ src/print.rs
@@ -0,0 +1,11 @@
+pub fn run() {
+    // Print to console
+    println!("Hello print.rs file");
+
+    // Basic Formatting
+    println!("{} is from {}", "Allen", "Sydney");
+
+    // Positional Arguments
+  
+    println!("{1} is from {0} and likes to {2}", "Allen", "Sydney", "code")
+}
